83ec4301e3d42ae601054ef3bd4402dcc351b8d4
Brishav Kuikel <brishav.kuikel@citytech.global>
2025-05-06 17:43:31 +0545

    kafka producer added for producing order message

--- order-service/kafka/producer.go
+++ order-service/kafka/producer.go
@@ -1,1 +1,36 @@
 package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+var writer *kafka.Writer
+
+func InitKafkaWriter(broker, topic string) {
+
+	writer = kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  []string{broker},
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	})
+}
+
+func PublishOrder(order any) {
+	msgBytes, err := json.Marshal(order)
+	if err != nil {
+		log.Println("Failed to marshal order:", err)
+		return
+	}
+
+	err = writer.WriteMessages(context.Background(), kafka.Message{
+		Key:   []byte("order"),
+		Value: msgBytes,
+	})
+	if err != nil {
+		log.Println("Failed to publish message:", err)
+	}
+}
--- order-service/main.go
+++ order-service/main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"event-driven-order-system/order-service/db"
 	"event-driven-order-system/order-service/handlers"
+	"event-driven-order-system/order-service/kafka"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Failed to load env or not env file found")
+	}
+
+	broker := os.Getenv("BROKER")
+
 	db.InitDB()
+	kafka.InitKafkaWriter(broker, "orders")
 	http.HandleFunc("/order", handlers.HandleOrder)
 	http.HandleFunc("/delete", handlers.HandleDelete)
 	log.Println("Order Service running on port 8080...")
