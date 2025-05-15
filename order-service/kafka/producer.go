276b2dc310da7e49af1448094352e7efc60689d2
Brishav Kuikel <brishav.kuikel@citytech.global>
2025-05-15 10:42:18 +0545

    in kafka producer auto create topic added but  not working

--- order-service/kafka/producer.go
+++ order-service/kafka/producer.go
@@ -12,6 +12,8 @@ var writer *kafka.Writer
 
 func InitKafkaWriter(broker, topic string) {
 
+	// ensureTopicExists(broker, topic)
+
 	writer = kafka.NewWriter(kafka.WriterConfig{
 		Brokers:  []string{broker},
 		Topic:    topic,
@@ -34,3 +36,37 @@ func PublishOrder(order any) {
 		log.Println("Failed to publish message:", err)
 	}
 }
+
+// func ensureTopicExists(broker, topic string) {
+// 	conn, err := kafka.Dial("tcp", broker)
+// 	if err != nil {
+// 		log.Fatalf("❌ Failed to connect to Kafka broker: %v", err)
+// 	}
+// 	defer conn.Close()
+
+// 	controller, err := conn.Controller()
+// 	if err != nil {
+// 		log.Fatalf("❌ Failed to get controller: %v", err)
+// 	}
+
+// 	controllerConn, err := kafka.Dial("tcp", controller.Host+":"+strconv.Itoa(controller.Port))
+// 	if err != nil {
+// 		log.Fatalf("❌ Failed to connect to controller: %v", err)
+// 	}
+// 	defer controllerConn.Close()
+
+// 	topicConfigs := []kafka.TopicConfig{
+// 		{
+// 			Topic:             topic,
+// 			NumPartitions:     1,
+// 			ReplicationFactor: 1,
+// 		},
+// 	}
+
+// 	err = controllerConn.CreateTopics(topicConfigs...)
+// 	if err != nil {
+// 		log.Printf("⚠️ Could not create topic %s (maybe it already exists): %v", topic, err)
+// 	} else {
+// 		log.Printf("✅ Topic '%s' created successfully", topic)
+// 	}
+// }
